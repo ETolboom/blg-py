9812856e79d8563e032dcbf18d75f164762bb0be
ETolboom <e.s.tolboom@student.tue.nl>
2025-12-02 11:34:32 +0100

    Avoid adding ANSI-escape codes to strings when returning the property name.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,12 @@ mod bpmn;
 pub mod model_checking;
 pub mod states;
 
-use pyo3::prelude::*;
 use crate::bpmn::collaboration::Collaboration;
 use crate::bpmn::reader;
 use crate::bpmn::reader::UnsupportedBpmnElementsError;
 pub use crate::model_checking::properties::{ModelCheckingResult, Property};
+use colored::Colorize;
+use pyo3::prelude::*;
 
 pub fn run(collaboration: &Collaboration, properties: Vec<Property>) -> ModelCheckingResult {
     collaboration.explore_state_space(properties)
@@ -43,7 +44,12 @@ pub struct PyProperty {
 #[pymethods]
 impl PyProperty {
     #[new]
-    fn new(property_name: String, fulfilled: bool, problematic_elements: Vec<String>, description: String) -> Self {
+    fn new(
+        property_name: String,
+        fulfilled: bool,
+        problematic_elements: Vec<String>,
+        description: String,
+    ) -> Self {
         PyProperty {
             property_name,
             fulfilled,
@@ -73,89 +79,85 @@ fn type_to_description(property: &Property) -> String {
 fn analyze_safeness(model: &str) -> PyResult<PyProperty> {
     match read_bpmn_from_string(model) {
         Ok(collaboration) => {
-            let mut property_result = run(&collaboration, vec![
-                Property::Safeness,
-            ]);
+            let mut property_result = run(&collaboration, vec![Property::Safeness]);
 
             let result = property_result.property_results.remove(0);
 
             Ok(PyProperty {
-                        property_name: result.property.to_string(),
-                        fulfilled: result.fulfilled,
-                        problematic_elements: result.problematic_elements,
-                        description: type_to_description(&result.property)
+                property_name: "Safeness".to_string(),
+                fulfilled: result.fulfilled,
+                problematic_elements: result.problematic_elements,
+                description: type_to_description(&result.property),
             })
-        },
-        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string()))
+        }
+        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            err.to_string(),
+        )),
     }
 }
-
 
 #[pyfunction]
 fn analyze_dead_activities(model: &str) -> PyResult<PyProperty> {
     match read_bpmn_from_string(model) {
         Ok(collaboration) => {
-            let mut property_result = run(&collaboration, vec![
-                Property::NoDeadActivities,
-            ]);
+            let mut property_result = run(&collaboration, vec![Property::NoDeadActivities]);
 
             let result = property_result.property_results.remove(0);
 
             Ok(PyProperty {
-                property_name: result.property.to_string(),
+                property_name: "No dead activities".to_string(),
                 fulfilled: result.fulfilled,
                 problematic_elements: result.problematic_elements,
-                description: type_to_description(&result.property)
+                description: type_to_description(&result.property),
             })
-        },
-        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string()))
+        }
+        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            err.to_string(),
+        )),
     }
 }
-
 
 #[pyfunction]
 fn analyze_option_to_complete(model: &str) -> PyResult<PyProperty> {
     match read_bpmn_from_string(model) {
         Ok(collaboration) => {
-            let mut property_result = run(&collaboration, vec![
-                Property::OptionToComplete,
-            ]);
+            let mut property_result = run(&collaboration, vec![Property::OptionToComplete]);
 
             let result = property_result.property_results.remove(0);
 
             Ok(PyProperty {
-                property_name: result.property.to_string(),
+                property_name: "Option to complete".to_string(),
                 fulfilled: result.fulfilled,
                 problematic_elements: result.problematic_elements,
-                description: type_to_description(&result.property)
+                description: type_to_description(&result.property),
             })
-        },
-        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string()))
+        }
+        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            err.to_string(),
+        )),
     }
 }
-
 
 #[pyfunction]
 fn analyze_proper_completion(model: &str) -> PyResult<PyProperty> {
     match read_bpmn_from_string(model) {
         Ok(collaboration) => {
-            let mut property_result = run(&collaboration, vec![
-                Property::ProperCompletion,
-            ]);
+            let mut property_result = run(&collaboration, vec![Property::ProperCompletion]);
 
             let result = property_result.property_results.remove(0);
 
             Ok(PyProperty {
-                property_name: result.property.to_string(),
+                property_name: "Proper completion".to_string(),
                 fulfilled: result.fulfilled,
                 problematic_elements: result.problematic_elements,
-                description: type_to_description(&result.property)
+                description: type_to_description(&result.property),
             })
-        },
-        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(err.to_string()))
+        }
+        Err(err) => Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+            err.to_string(),
+        )),
     }
 }
-
 
 // This is the module "entrypoint" for Python
 #[pymodule]
@@ -165,14 +167,17 @@ fn blg(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(analyze_option_to_complete, m)?)?;
     m.add_function(wrap_pyfunction!(analyze_proper_completion, m)?)?;
     m.add_function(wrap_pyfunction!(analyze_safeness, m)?)?;
-    
-    m.add("__all__", vec![
-        "PyProperty",
-        "analyze_dead_activities",
-        "analyze_option_to_complete",
-        "analyze_proper_completion",
-        "analyze_safeness",
-    ])?;
+
+    m.add(
+        "__all__",
+        vec![
+            "PyProperty",
+            "analyze_dead_activities",
+            "analyze_option_to_complete",
+            "analyze_proper_completion",
+            "analyze_safeness",
+        ],
+    )?;
 
     Ok(())
 }
